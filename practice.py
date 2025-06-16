da2ac2fe71306ed2475a967e0f2a0dfa7ae05a4a
ADITYA SHETTY <161926174+ShettyAditya24@users.noreply.github.com>
2025-06-16 17:53:27 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+a=int(input('Enter a first number:'))
+b=int(input("Enter a second number:"))
+
+Addition=a + b
+Subtraction=a - b
+Multiplication=a*b
+Division=a/b
+
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
+
+
---
+++ task2.py
@@ -0,0 +1,6 @@
+# Task 2
+
+fname=input("Enter first name: ")
+lname=input("Enter last name: ")
+
+print("Hello,",fname+lname+"!","Welcome to the python program." )
